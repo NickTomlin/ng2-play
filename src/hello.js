58bf4abe195bf1576427f9fd3c2ee2bbfca26d56
Nick Tomlin <nick.tomlin@gmail.com>
2015-05-01 10:44:55 -0500

    Get to "user input" section of quickstart

--- src/hello.js
+++ src/hello.js
@@ -1,19 +1,51 @@
-import {Component, View, bootstrap, If} from 'angular2/angular2';
+import {Inject, Component, View, bootstrap, If, For} from 'angular2/angular2';
 
-@Component({
-    selector: 'hello'  //TODO: default to camel-cased class name if not provided?
-})
-@View({
-    template: `<span *if="name">Hello, {{name}}!</span>`,
-    directives: [If]
-})
-export class Hello {
-    name: string = 'World';
-    constructor() {
-        setTimeout(() => {
-          this.name = 'NEW World'
-        }, 2000);
-    }
+function FriendsService() {
+  this.names = ["Alice", "Aarav", "Martín", "Shannon", "Ariana", "Kai"];
+  this.add = function (name) {
+    this.names.push(name);
+  };
 }
 
-bootstrap(Hello);
+@View({
+  template: `
+    <p>My name: {{ myName }}</p>
+    <p>Friends:</p>
+    <ul>
+      <li *for="#name of names">
+        {{ name }}
+      </li>
+    </ul>
+    <p *if="names.length > 3">You have many friends!</p>
+    <input #friendtext type="text" (keyup)="doneTyping($event)">
+    <button (click)="addFriend(friendtext.value)">Add Todo</button>
+  `,
+  directives: [For, If]
+})
+@Component({
+  selector: "display",
+  injectables: [FriendsService]
+})
+export class DisplayComponent {
+  constructor(friends) {
+    this.myName = "Alice";
+    this.names = friends.names;
+    this.friends = friends;
+  }
+
+  doneTyping ($event) {
+    if ($event.which === 13) {
+      this.addFriend($event.target.value);
+      $event.target.value = null;
+    }
+  }
+
+  addFriend (name) {
+    this.friends.add(name);
+  }
+}
+
+DisplayComponent.parameters = [[FriendsService]];
+
+bootstrap(DisplayComponent);
+
